b3fe5eeca42d4f6ab608081db0169e32fbc373a9
Gabriel Padilla <cgabrielpadilla24@gmail.com>
2025-12-29 09:34:25 -0600

    Se creo el archivo inicial del servidor

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, make_response, redirect, render_template, request
+
+from flask_restful import Api, Resource
+from rutas import crear_rutas
+
+app = Flask(__name__)#creamos un objeto de tipo flask
+#creamos un objeto de tipo Api y como argumento le pasamos el objeto app
+#el parametro/argumento que espera recibir es el objeto de flask
+api = Api(app)
+#la api sera el rpopgrama que se comunique con el dispositivo fisico
+
+crear_rutas(api)
+
+if __name__ == '__main__':
+    app.run()
